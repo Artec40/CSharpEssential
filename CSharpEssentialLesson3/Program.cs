7176354e5dc6f461328a7728696bda9c37346920
dnwdark <Artec40@gmail.com>
2019-08-31 11:07:35 +0500

    ывыф

---
+++ CSharpEssentialLesson3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpEssentialLesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
