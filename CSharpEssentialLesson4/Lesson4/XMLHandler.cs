a7e20a94b1410e87cc1de660bad7c16f33b44f8b
dnwdark <Artec40@gmail.com>
2019-09-19 19:04:14 +0500

    Lesson 4. Абстрактные классы и интерфейсы

---
+++ CSharpEssentialLesson4/Lesson4/XMLHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4_1
+{
+    /// Создайте класс AbstractHandler.
+    /// В теле класса создать методы void Open(), void Create(), void Change(), void Save().
+    /// Создать производные классы XMLHandler, TXTHandler, DOCHandler от базового класса
+    /// AbstractHandler.
+    /// Написать программу, которая будет выполнять определение документа и для каждого формата должны
+    /// быть метода открытия, создания, редактирования, сохранения определенного формата документа.
+    public class XMLHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("XMLHandler Open");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("XMLCreate");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("XMLChange");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("XMLSave");
+        }
+    }
+}
