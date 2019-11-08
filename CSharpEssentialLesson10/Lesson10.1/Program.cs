0b0eb6c9426c8712f68c05af049be39ddf462794
dnwdark <Artec40@gmail.com>
2019-11-08 13:03:13 +0500

    repair Repository

---
+++ CSharpEssentialLesson10/Lesson10.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Размерность массива ");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            var list = new MyList<int>();
+            for (int i = 0; i < userValue; i++)
+            {
+                list.Add(i);
+                Console.WriteLine("Элемент массива {0}", list[i]);
+            }
+
+            Console.Write("Номер массива ");
+            userValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(list[userValue]);
+
+            Console.WriteLine("Размер массива {0}", list.arrayLength);
+        }
+    }
+}
