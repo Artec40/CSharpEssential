6710a68a80d0311d264ad2e3cbc00789268e569f
dnwdark <Artec40@gmail.com>
2019-11-12 15:49:33 +0500

    Lesson 12

---
+++ CSharpEssentialLesson12/Lesson12/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Presenter(this);
+        }
+
+        public EventHandler AddEvent = null;
+        public EventHandler SubEvent = null;
+        public EventHandler MulEvent = null;
+        public EventHandler DivEvent = null;
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEvent.Invoke(sender, e);
+        }
+
+        private void SubButton_Click(object sender, RoutedEventArgs e)
+        {
+            SubEvent.Invoke(sender, e);
+        }
+
+        private void MulButton_Click(object sender, RoutedEventArgs e)
+        {
+            MulEvent.Invoke(sender, e);
+        }
+
+        private void DevButton_Click(object sender, RoutedEventArgs e)
+        {
+            DivEvent.Invoke(sender, e);
+        }
+    }
+}
---
+++ CSharpEssentialLesson12/Lesson12/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    class Model
+    {
+        public string Plus(int a, int b)
+        {
+            return $"Result= { (a + b).ToString()}";
+        }
+
+        public string Sub(int a, int b)
+        {
+            return $"Result= { (a - b).ToString()}";
+        }
+
+        public string Mul(int a, int b)
+        {
+            return $"Result= { (a * b).ToString()}";
+        }
+
+        public string Div(int a, int b)
+        {
+            if (b != 0)
+                return $"Result= {((double)a / b).ToString()}";
+            throw new Exception("Деление на ноль");
+        }
+    }
+}
