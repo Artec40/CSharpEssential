8e95126e3d402d82300d09a81f4f4f43c5911f2f
dnwdark <Artec40@gmail.com>
2019-11-14 23:34:20 +0500

    Lesson14. "Коллекции". Выполнено на основе MyList из урока 10.

---
+++ CSharpEssentialLesson14/Lesson_14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Размерность массива ");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            var list = new MyList<int>();
+            for (int i = 0; i < userValue; i++)
+            {
+                list.Add(i);
+            }
+
+            // Добавлен foreach.
+            foreach (int item in list)
+            {
+                Console.WriteLine("Элемент массива {0}", item);
+            }
+
+            Console.Write("Номер массива ");
+            userValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(list[userValue]);
+
+            Console.WriteLine("Размер массива {0}", list.listLength);
+        }
+    }
+}
