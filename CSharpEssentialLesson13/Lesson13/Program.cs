41c10cc935dda52087b9331389d93fe539feb6e3
dnwdark <Artec40@gmail.com>
2019-11-14 23:37:10 +0500

    Lesson 13 - про матрицу. На данный момент не готов (14.11.19)

---
+++ CSharpEssentialLesson13/Lesson13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+
+            while (true)
+            {
+                new Thread(matrix.StartMatrix).Start();
+            }
+        }
+    }
+}
