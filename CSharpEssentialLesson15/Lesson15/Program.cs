3b4617dbb47d63d2de36103c64d7a9e4d9c0bcf3
dnwdark <Artec40@gmail.com>
2019-11-14 23:35:51 +0500

    Lesson 15. "Обработка исключений".

---
+++ CSharpEssentialLesson15/Lesson15/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string position;
+            string yearNotChecked;
+            int yearChecked;
+
+            Worker[] company = new Worker[5];
+
+            for (int i = 0; i < company.Length; i++)
+            {
+                Console.Write("Worker's name= ");
+                name = Console.ReadLine();
+                Console.Write("Worker's position= ");
+                position = Console.ReadLine();
+                Console.Write("Worker's year= ");
+                yearNotChecked = Console.ReadLine();
+
+                try
+                {
+                    yearChecked = Convert.ToInt32(yearNotChecked);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong year value");
+                    Console.WriteLine(e.Message);
+                    yearChecked = 0;
+                }
+
+                company[i] = new Worker(name, position, yearChecked);
+            }
+
+            company = company.OrderBy(worker => worker.Name).ToArray<Worker>();
+
+            Console.Write("Work experience more then ");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            for (int j = 0; j < company.Length; j++)
+            {
+                company[j].FindWorkExperienceMoreThan(userValue);
+            }
+        }
+    }
+}
---
+++ CSharpEssentialLesson15/Lesson15/Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson15
+{
+    struct Worker
+    {
+        string name;
+        string position;
+        int year;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Position
+        {
+            get
+            {
+                return position;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+        }
+
+        public Worker(string name, string position, int year)
+        {
+            this.name = name;
+            this.position = position;
+            this.year = year;
+        }
+
+        public void FindWorkExperienceMoreThan(int experience)
+        {
+            if (Year >= experience)
+                Console.WriteLine($"Worker: name {Name}, position {Position}, year {Year}");
+        }
+    }
+}
+
