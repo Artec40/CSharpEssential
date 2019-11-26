c7c684d8c858f74a6a179355a57541b9893499cc
dnwdark <Artec40@gmail.com>
2019-11-27 00:21:55 +0500

    Lesson 16. Клонирование

---
+++ CSharpEssentialLesson16/Lesson16.1/House.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson16._1
+{
+    class House
+    {
+        private string name;
+        private int year;
+
+        public string Name { get; set; }
+        public string Year { get; set; }
+
+        public House(string name, string year)
+        {
+            this.Name = name;
+            this.Year = year;
+        }
+
+        // Поверхностное клонирование.
+        public House Clone()
+        {
+            return this;
+        }
+
+        // Глубокое клонирование.
+        public House DeepClone()
+        {
+            return this.MemberwiseClone() as House;
+        }
+
+        public House ChangeClone()
+        {
+            this.Name = "Another name";
+            this.Year = "Another year";
+            return this;
+        }
+    }
+}
---
+++ CSharpEssentialLesson16/Lesson16.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House newHouse = new House("Original name", "Original year");
+
+            Console.WriteLine("Clone");
+            House cloneHouse = newHouse.Clone();
+            cloneHouse.ChangeClone();
+            Console.WriteLine($"Original name:{newHouse.Name}, Original year:{newHouse.Year}");
+            Console.WriteLine($"Clone name:{cloneHouse.Name}, Clone year:{cloneHouse.Year}");
+            Console.WriteLine();
+
+            // Возвращаем значение оригинала после поверхностного копирования.
+            newHouse.Name = "Original name";
+            newHouse.Year = "Original year";
+
+            Console.WriteLine("Deep Clone");
+            House deepCloneHouse = newHouse.DeepClone();
+            deepCloneHouse.ChangeClone();
+            Console.WriteLine($"Original name:{newHouse.Name}, Original year:{newHouse.Year}");
+            Console.WriteLine($"Clone name:{deepCloneHouse.Name}, Clone year:{deepCloneHouse.Year}");
+        }
+    }
+}
