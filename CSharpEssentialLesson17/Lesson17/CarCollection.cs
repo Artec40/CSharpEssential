17ddb6df7b6c09ac96e87e73f618403883fc2a99
dnwdark <Artec40@gmail.com>
2019-12-11 23:21:18 +0500

    Lesson 17. Анонимные и динамические типы. LINQ

---
+++ CSharpEssentialLesson17/Lesson17/CarCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson17
+{
+    class CarCollection
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public CarCollection(string mark, string model, int year, string color)
+        {
+            this.Mark = mark;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+    }
+}
