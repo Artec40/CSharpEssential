de9ee61970ab8b41ea801fc773ca43fd0f02fdbd
dnwdark <Artec40@gmail.com>
2019-12-14 12:18:26 +0500

    Исправил в соответствии с замечаниями.

--- CSharpEssentialLesson17/Lesson17/ClientCollection.cs
+++ CSharpEssentialLesson17/Lesson17/ClientCollection.cs
@@ -4,17 +4,11 @@ using System.Text;
 
 namespace Lesson17
 {
-    class ClientCollection
+    class ClientCollection : List<Client>
     {
-        public string Mark { get; set; }
-        public string Name { get; set; }
-        public long PhoneNumber { get; set; }
-
-        public ClientCollection(string mark, string name, long phoneNumber)
+        public void Add(string mark, string name, long phoneNumber)
         {
-            this.Mark = mark;
-            this.Name = name;
-            this.PhoneNumber = phoneNumber;
+            base.Add(new Client(mark, name, phoneNumber));
         }
     }
 }
