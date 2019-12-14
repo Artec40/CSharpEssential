9ff915ef75b2d38b81f78e6694328c3fc2b80410
dnwdark <Artec40@gmail.com>
2019-12-14 12:26:56 +0500

    Я комичу в мастер, я молодец

---
+++ CSharpEssentialLesson17/Lesson17/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson17
+{
+    class Car
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public Car(string mark, string model, int year, string color)
+        {
+            this.Mark = mark;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+    }
+}
---
+++ CSharpEssentialLesson17/Lesson17/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson17
+{
+    class Client
+    {
+        public string Mark { get; set; }
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public Client(string mark, string name, long phoneNumber)
+        {
+            this.Mark = mark;
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
