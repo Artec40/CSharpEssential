ee32b38e4782dcf2778de879a21f4e3e42f0c173
dnwdark <Artec40@gmail.com>
2019-12-20 21:49:39 +0500

    Lesson 18 rebase

---
+++ CSharpEssentialLesson18/ClassLibrary/PublicClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class PublicClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello from Solution Task2, publicClass");
+        }
+    }
+}
---
+++ CSharpEssentialLesson18/Task2/DerivedClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary;
+
+namespace Task2
+{
+    class DerivedClass : BaseClass
+    {
+        
+    }
+}
